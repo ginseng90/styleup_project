980819ef9b500b5fda34cdf29980853a2f1803ac
ginseng90 <ginseng90@naver.com>
2017-09-07 18:25:14 +0900

    2017-09-07-종강

---
+++ js/function.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017-09-06.
+ */
+//
+//$(function(){
+//  $('.su1-header-visual-logo-move').animate({
+//    top:100,
+//    left:200
+//  },function(){
+//    $('.su1-header-visual-logo-move').animate({
+//      top:50,
+//      left:50
+//    });
+//  });
+//});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIlNVLTEuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImZ1bmN0aW9uLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIENyZWF0ZWQgYnkgQWRtaW5pc3RyYXRvciBvbiAyMDE3LTA5LTA2LlxyXG4gKi9cclxuLy9cclxuLy8kKGZ1bmN0aW9uKCl7XHJcbi8vICAkKCcuc3UxLWhlYWRlci12aXN1YWwtbG9nby1tb3ZlJykuYW5pbWF0ZSh7XHJcbi8vICAgIHRvcDoxMDAsXHJcbi8vICAgIGxlZnQ6MjAwXHJcbi8vICB9LGZ1bmN0aW9uKCl7XHJcbi8vICAgICQoJy5zdTEtaGVhZGVyLXZpc3VhbC1sb2dvLW1vdmUnKS5hbmltYXRlKHtcclxuLy8gICAgICB0b3A6NTAsXHJcbi8vICAgICAgbGVmdDo1MFxyXG4vLyAgICB9KTtcclxuLy8gIH0pO1xyXG4vL30pOyJdfQ==
---
+++ js_src/SU-1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017-09-06.
+ */
+//
+//$(function(){
+//  $('.su1-header-visual-logo-move').animate({
+//    top:100,
+//    left:200
+//  },function(){
+//    $('.su1-header-visual-logo-move').animate({
+//      top:50,
+//      left:50
+//    });
+//  });
+//});
